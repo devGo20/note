ca333e07387b1a15b1b5e814517aca1167588aeb
Enubin Go <enubin.go@reebonz.co.kr>
2023-04-19 15:16:10 +0900

    [ADD] product-link

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,17 @@
-import React from 'react';
+import Link from 'next/link';
+const products = ['shirt', 'pants', 'skirt', 'shoes'];
 
 export default function ProductsPage() {
-  return <div>제품 소개 페이지!</div>;
+  return (
+    <>
+      <h1>제품 소개 페이지!</h1>
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>
+            <Link href={`/products/${product}`}>{product}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
