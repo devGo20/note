941d7f819a0c8c793bc353ae405b0909c40f0c25
Enubin Go <enubin.go@reebonz.co.kr>
2023-04-19 14:53:52 +0900

    [ADD] Dynamic Routing

---
+++ src/app/products/[slug]/page.tsx
@@ -0,0 +1,21 @@
+import { notFound } from 'next/navigation';
+
+type Props = {
+  params: {
+    slug: string; // []안 slug가 key
+  };
+};
+export default function PantsPage({ params }: Props) {
+  if (params.slug === 'nothing') {
+    notFound(); // not-found 호출 함수
+  }
+  return <h1>{params.slug} 소개 페이지!</h1>;
+}
+
+export function generateStaticParams() {
+  // build할 때 명시된 것을 미리 만들어두는 것
+  const proucts = ['pants', 'skirt'];
+  return proucts.map((product) => ({
+    slug: product,
+  }));
+}
