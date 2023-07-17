701ccadf8f2f9a0f6f317bb543ee2094294d3dc8
eunbin go <eunbin.go@reebonz.co.kr>
2023-07-17 17:06:38 +0900

    [ADD] home image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,21 @@
 import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import os from 'os'; // node APIs
+import os from 'os'; // 노드 APIs
 import Counter from '@/components/Counter';
 
-const inter = Inter({ subsets: ['latin'] });
-
 export default function Home() {
-  console.log('hi!? - server');
+  console.log('안녕! - 서버');
   console.log(os.hostname());
 
   return (
     <>
-      <h1>홈페이지다!</h1>
+      <h1>홈페이지다!!</h1>
       <Counter />
+      <Image
+        src='https://images.unsplash.com/photo-1441986300917-64674bd600d8'
+        alt='shop'
+        width={400}
+        height={400}
+      />
     </>
   );
 }
