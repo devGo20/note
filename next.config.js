578883150165542e9994be5f9b1104b50b07d57b
eunbin go <eunbin.go@reebonz.co.kr>
2023-07-17 17:46:10 +0900

    [ADD] redirects config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,22 @@ const nextConfig = {
     appDir: true,
   },
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "images.unsplash.com" }],
+    remotePatterns: [{ protocol: 'https', hostname: 'images.unsplash.com' }],
+  },
+  async redirects() {
+    // 경로가 바뀜
+    return [
+      {
+        source: '/products/deleted_forever', // 이 경로로 오면
+        destination: '/products', // 여기로 redirect
+        permanent: true, // 캐시해두고 다른 사용자가 해당 url로 온다면 products로 바로 redirect 가능!
+      },
+      {
+        source: '/products/deleted_temp',
+        destination: '/products',
+        permanent: false, // 영원히 redirect 아님
+      },
+    ];
   },
 };
 
